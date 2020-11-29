159303fa5c83af7eec8d0dae881ed93185d83ff7
Greg <gregcudzich@gmail.com>
2020-11-29 16:05:31 -0600

    readded package.json to ACSG542 directory due to serverside tasks needing usage of these npm modules to function, cleaned up DB pull and save for project component, added delete function to project component, added one extra service to handle the data being passed between the project and tasks component, this way we don't have to include the project name and ID in the url for safer usage, set up segue between projects and tasks component so now the task-root is displayed when a user clicks on a project.

--- theAmazingProject/src/app/project-root/project-root.component.ts
+++ theAmazingProject/src/app/project-root/project-root.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectServiceService } from '../project-service.service';
+import { Router } from '@angular/router';
+import {DataHandlerService} from '../data-handler.service';
 
 @Component({
   selector: 'app-project-root',
@@ -11,18 +13,39 @@ export class ProjectRootComponent implements OnInit {
   public projects;
 
   constructor(
-    private projectServiceService: ProjectServiceService
+    private projectServiceService: ProjectServiceService,
+    private route: Router,
+    private dataPkg: DataHandlerService
   ) { }
 
 
   ngOnInit(): void {
     this.projectServiceService.getAllProjects().subscribe(returnProjects => {
-      this.projects = returnProjects.docs;
+      this.projects = returnProjects;
     });
+  }
+
+  viewProjectTasks(selectedProject): void {
+    this.dataPkg.projectId = selectedProject._id;
+    this.dataPkg.projectName = selectedProject.name;
+    this.route.navigate(['task-root']);
   }
   saveProjectItem(): void {
     this.projectServiceService.create(this.newProjectItem).subscribe(saveProjectItem => {
       this.projects.push(saveProjectItem);
+      // clears out text field on page for cleaner UI
+      this.newProjectItem = '';
+    });
+  }
+
+  deleteProjectItem(projectIdMarked, index): void {
+    // tslint:disable-next-line:no-shadowed-variable
+    this.projectServiceService.delete(projectIdMarked).subscribe(projectIdMarked => {
+
+      // tslint:disable-next-line:triple-equals
+      if (index != -1) {
+        this.projects.splice(index, 1);
+      }
     });
   }
 }
