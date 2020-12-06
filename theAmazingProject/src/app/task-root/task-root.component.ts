2c7c91d6fb01872f6b9028309eb30ed7a1627b8f
Greg <gregcudzich@gmail.com>
2020-12-06 13:45:12 -0600

    fixed NaN issue on page load for real time timer, fixed issue where starting one timer will start the other timers, fixed issue where starting one timer, then starting another timer and pressing STOP on the first timer, will stop the second timer and not stop the first timer.... tablerow issue pretty much.

--- theAmazingProject/src/app/task-root/task-root.component.ts
+++ theAmazingProject/src/app/task-root/task-root.component.ts
@@ -17,8 +17,8 @@ export class TaskRootComponent implements OnInit {
   public projectId;
   public newTaskItem;
   public returnedTasks;
-  public realTimeCounter;
-  private timerId;
+  public realTimeCounter = [];
+  private timerId = [];
   changeBackground = [];
   changeText = [];
 
@@ -36,6 +36,8 @@ export class TaskRootComponent implements OnInit {
       this.returnedTasks = returnedTasks;
       returnedTasks.forEach(x => this.changeBackground.push('main3'));
       returnedTasks.forEach(x => this.changeText.push('Start'));
+      returnedTasks.forEach(x => this.realTimeCounter.push(0));
+      returnedTasks.forEach(x => this.timerId.push(0));
     });
     this.simpleTimer.newTimer('1sec', 1, true);
   }
@@ -68,15 +70,16 @@ export class TaskRootComponent implements OnInit {
     if (this.changeBackground[index] === 'main3') {
       this.timerService.startTimer(taskIdActual);
       this.changeBackground[index] = 'main4';
-      this.timerId = this.simpleTimer.subscribe('1sec', () => this.realTimeCounter++);
+      this.timerId[index] = this.simpleTimer.subscribe('1sec', () => this.realTimeCounter[index]++);
     } else {
       this.timerService.stopTimer(taskIdActual);
       this.taskService.getAllTasks(this.projectId).subscribe(returnedTasks => {
         this.returnedTasks = returnedTasks;
       });
       this.changeBackground[index] = 'main3';
-      this.simpleTimer.unsubscribe(this.timerId);
-      this.realTimeCounter = 0;
+      this.simpleTimer.unsubscribe(this.timerId[index]);
+      this.timerId[index] = 0;
+      this.realTimeCounter[index] = 0;
     }
   }
 
@@ -91,11 +94,14 @@ export class TaskRootComponent implements OnInit {
 
   // tslint:disable-next-line:typedef
   secondsToHms(totalSeconds) {
-    if (totalSeconds <= 0) { return '00:00:00'; }
-    totalSeconds = Number(totalSeconds);
-    let h = Math.floor(totalSeconds / 3600);
-    let m = Math.floor(totalSeconds % 3600 / 60);
-    let s = Math.floor(totalSeconds % 3600 % 60);
-    return ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2);
+    if (totalSeconds <= 0) {
+      return '00:00:00';
+    } else {
+      totalSeconds = Number(totalSeconds);
+      let h = Math.floor(totalSeconds / 3600);
+      let m = Math.floor(totalSeconds % 3600 / 60);
+      let s = Math.floor(totalSeconds % 3600 % 60);
+      return ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2);
+    }
   }
 }
