e85320a17f84ab70a4d938f63d7e05767054083c
Greg <gregcudzich@gmail.com>
2020-12-05 19:28:55 -0600

    fixed issue with time not incrementing when saved to DB, it was updating instead of adding onto the current value, fixed pathing of files so they're not so scattered, added delete from background and text arrays, changed up start and stop timer call locations since if you start a timer from one row and then start another from a different row, it stopped the previous timer... whack.

--- theAmazingProject/src/app/_services/taskItem.service.ts
+++ theAmazingProject/src/app/_services/taskItem.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
-import {environment} from '../environments/environment';
+import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TaskServiceService {
+export class TaskItemService {
 
   constructor(private http: HttpClient) { }
 
